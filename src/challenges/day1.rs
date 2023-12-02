5af035c7e8fecae99b3694e61d4f40f8a5588a5d
Adrian Opsahl <adrian.opsahl@gmail.com>
2023-12-02 11:59:55 +0100

    day1 - task2

--- src/challenges/day1.rs
+++ src/challenges/day1.rs
@@ -1,9 +1,16 @@
 use axum::{extract::Path, response::IntoResponse, routing::get, Router};
+use tracing::info;
 
 pub fn routes() -> Router {
-    Router::new().route("/:num1/:num2", get(handler))
+    Router::new().route("/*bits", get(handler))
 }
 
-async fn handler(Path((num1, num2)): Path<(u32, u32)>) -> impl IntoResponse {
-    (num1 ^ num2).pow(3).to_string()
+async fn handler(Path(path): Path<String>) -> impl IntoResponse {
+    info!("pat: {path}");
+    path.split('/')
+        .filter_map(|segment| segment.parse::<u32>().ok())
+        .reduce(|acc, e| acc ^ e)
+        .expect("should be valid")
+        .pow(3)
+        .to_string()
 }
